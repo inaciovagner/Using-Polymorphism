378d93f618032431b2b6ad857dadaff98a8f3b36
Vagner de Souza <76208231+inaciovagner@users.noreply.github.com>
2021-01-23 12:57:59 +0000

    Using Polymorphism
    C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Polygon> polygons = new List<Polygon> ();
+            polygons.Add(new Polygon());
+            polygons.Add(new Rectangle());
+            polygons.Add(new Triangle());
+
+            foreach (Polygon p in polygons)
+            {
+              p.Draw();
+            }
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace polymorphism
+{
+    class Rectangle : Polygon
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing: Rectangle");
+        }
+    }
+}
